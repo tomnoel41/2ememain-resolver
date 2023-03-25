21566c7b4c9dfbc6d2ec4d623e77d72e77e1db2e
Tom Noel <tom.noel@atib.network>
2023-03-25 16:30:30 +0100

    Add commerce.json for configuration

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cheerio = require('cheerio');
 const axios = require('axios');
+const fs = require('fs');
 const rateLimit = require("express-rate-limit");
 
 const rlAPI = rateLimit({
@@ -15,48 +16,21 @@ const rlAPI = rateLimit({
 const app = express();
 app.use(rlAPI);
 
+const commerceData = JSON.parse(fs.readFileSync('commerce.json'));
+
 app.get('/search/:product', async (req, res) => {
   const productName = req.params.product;
   const prices = {};
 
-  // Recherche sur Alternate
-  try {
-    const alternate = await axios.get(`https://fr.alternate.be/html/search.html?query=${productName}`);
-    const $ = cheerio.load(alternate.data);
-    const price = $('span.price').first().text().replace(/\s/g, '');
-    prices.alternate = price;
-  } catch (error) {
-    console.error(`Erreur lors de la recherche sur Alternate: ${error}`);
-  }
-
-  // Recherche sur LDLC
-  try {
-    const ldlc = await axios.get(`https://www.ldlc.com/recherche/${productName}`);
-    const $ = cheerio.load(ldlc.data);
-    const price = $('div.price').first().text().replace(/\s/g, '');
-    prices.ldlc = price;
-  } catch (error) {
-    console.error(`Erreur lors de la recherche sur LDLC: ${error}`);
-  }
-
-  // Recherche sur TopAchat
-  try {
-    const topachat = await axios.get(`https://www.topachat.com/search/${productName}`);
-    const $ = cheerio.load(topachat.data);
-    const price = $('span.offer-price__price').first().text().replace(/\s/g, '');
-    prices.topachat = price;
-  } catch (error) {
-    console.error(`Erreur lors de la recherche sur TopAchat: ${error}`);
-  }
-
-  // Recherche sur RueDuCommerce
-  try {
-    const rueducommerce = await axios.get(`https://www.rueducommerce.fr/r/${productName}`);
-    const $ = cheerio.load(rueducommerce.data);
-    const price = $('span.item__price--new-box').first().text().replace(/\s/g, '');
-    prices.rueducommerce = price;
-  } catch (error) {
-    console.error(`Erreur lors de la recherche sur RueDuCommerce: ${error}`);
+  for (const commerce of commerceData) {
+    try {
+      const response = await axios.get(commerce.searchUrl + productName);
+      const $ = cheerio.load(response.data);
+      const price = $(commerce.priceSelector).first().text().replace(/\s/g, '');
+      prices[commerce.name] = price;
+    } catch (error) {
+      console.error(`Error while searching on ${commerce.name}: ${error}`);
+    }
   }
 
   // Calcul de la moyenne des prix
