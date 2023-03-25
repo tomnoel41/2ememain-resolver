7483f943ea65e9624a0efb069fbf3bbbc7c33dfa
Tom Noel <tom.noel@atib.network>
2023-03-25 17:08:11 +0100

    Add other config

--- api.js
+++ api.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const rateLimit = require("express-rate-limit");
 
 const rlAPI = rateLimit({
-  windowMs: 1 * 60 * 1000, // 2 minutes
-  max: 10, // limit each IP to 45 requests per windowMs
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 45, // limit each IP to 45 requests per windowMs
   message: { error: "Too many requests, please try again later" },
   handler: (req, res, next) => {
     res.status(429).json({ error: "Too many requests, please try again later" });
