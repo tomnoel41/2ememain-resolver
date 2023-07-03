5b18e26343993734db7cf209a3a718d16be31e5e
Tom Noel <tom.noel@atib.network>
2023-07-03 15:16:25 +0200

    2ememain-resolver

--- api.js
+++ api.js
@@ -9,32 +9,36 @@ const app = express();
 const instance = axios.create();
 let previousAds = [];
 
-const productName = "Peugot Fox Classe A";
+const productName = "Peugot Fox Classe A"; // Mettre le nom du produit
 const name = '2ememain';
 const searchUrl = 'https://www.2ememain.be/q/';
-const endUrl = '/#Language:all-languages|sortBy:SORT_INDEX|sortOrder:DECREASING|searchInTitleAndDescription:true';
 const priceSelector = 'span.hz-text-price-label';
 const nameSelector = 'h3.hz-Listing-title';
 const descriptionSelector = 'p.hz-Listing-description';
 const adSelector = 'li.hz-Listing';
-const webhook = new WebhookClient({ id: '1125138318670188544', token: '_pMXrwDEsT3KUKOf6DsLCvtHv9UTdXniEsQujFXQRQArk3IFlCr38jlwRASXOlrI_I60' });
+const webhook = new WebhookClient({ id: '', token: '' }); // Mettre le l'id et le token (via une URL, l'id est avant le / et le token après) du webhook Discord
 
 app.get('/search/', async (req, res) => {
+  
   const ads = [];
 
   try {
+
     const response = await instance.get(searchUrl + productName);
     const $ = cheerio.load(response.data);
 
     $(adSelector).each((index, element) => {
+
       const price = $(element).find(priceSelector).first().text().replace(/\s/g, '');
       const name = $(element).find(nameSelector).first().text();
       const description = $(element).find(descriptionSelector).first().text();
 
       const ad = {
+
         price: price,
         name: name,
         description: description
+
       };
 
       if (!previousAds.some(prevAd => prevAd.name === ad.name && prevAd.description === ad.description)) {
@@ -45,30 +49,39 @@ app.get('/search/', async (req, res) => {
       
 
       ads.push(ad);
+
     });
 
   } catch (error) {
+
     console.error(`Error while searching on ${name}: ${error}`);
+
   }
 
   previousAds = ads;
 
   const response = {
+
     product: productName,
     totalAds: ads.length,
     ads: ads
+    
   };
 
   res.json(response);
 });
 
 app.listen(port, () => {
+
   console.log(`Serveur lancé, port : ${port}`);
+
 });
 
 // Fonction pour vérifier les nouvelles annonces
 async function checkForNewAds() {
+
   const response = await axios.get(`http://localhost:${port}/search/`);
+
 }
 
 // Appeler la fonction pour la première fois
